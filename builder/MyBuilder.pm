f2c60ba6c83b3920cb55c6a3fff929b2fe1ae54d
Yoshihiro Sasaki <ysasaki@cpan.org>
2013-06-18 04:23:29 +0000

    Import of YSASAKI/Text-Sass-XS-0.04 from CPAN.
    gitpan-cpan-distribution: Text-Sass-XS
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         YSASAKI/Text-Sass-XS-0.04.tar.gz
    gitpan-cpan-author:       YSASAKI
    gitpan-cpan-maturity:     released

--- builder/MyBuilder.pm
+++ builder/MyBuilder.pm
@@ -7,12 +7,26 @@ sub new {
     my ( $self, %args ) = @_;
     $self->SUPER::new(
         %args,
-        c_source => ['libsass'],
-        config   => {
-            cc => 'g++',
-            ld => 'g++',
-        }
+        c_source             => ['libsass'],
+        extra_compiler_flags => [ '-x', 'c++' ],
+        extra_linker_flags   => ['-lstdc++'],
     );
+}
+
+# Copy from Module::Build::Pluggable::XSUtil
+sub HOOK_configure {
+    my $self = shift;
+
+    # c++ options
+    if ( $self->{'c++'} ) {
+        require ExtUtils::CBuilder;
+        my $cbuilder = ExtUtils::CBuilder->new( quiet => 1 );
+        $cbuilder->have_cplusplus or do {
+            warn
+                "This environment does not have a C++ compiler(OS unsupported)\n";
+            exit 0;
+        };
+    }
 }
 
 1;
